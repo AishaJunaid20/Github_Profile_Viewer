a3cc58799b3df1ba0e65da494bf67b7c9fefbd49
AishaJunaid20 <ayeshajunaidmemon@gmail.com>
2024-11-27 18:09:34 +0500

    Add files via upload

---
+++ src/components/ui/spinner.tsx
@@ -0,0 +1,8 @@
+const Spinner = () => {
+    return (
+      <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-blue-500"></div>
+    );
+  };
+  
+  export { Spinner }; // Named export
+  
